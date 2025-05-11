13a776f2d85f180998fdc18316beccb0882ca416
Mahdi Altoubli <mahdiatubly@gmail.com>
2025-05-12 00:00:14 +0300

    Create The base structure of Nawarah.js

---
+++ mini-framework/core/framework.js
@@ -0,0 +1,11 @@
+import { UI } from './ui.js';
+import { AppNavigator } from './navigation.js';
+import { AppStore } from './store.js';
+import { EventManager } from './events.js';
+
+export const CoreFramework = {
+    UI,
+    AppNavigator,
+    AppStore,
+    EventManager
+};
